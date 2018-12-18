48f506c685d7947038f0bdce1f78bfb82231cbe3
Enrico <enrico.berthod@studenti.unitn.it>
2018-12-18 17:05:00 +0100

    commenti utili

--- area.js
+++ area.js
@@ -11,6 +11,18 @@ function getArea(v) {
 /*
 var a =[]
 console.log(getArea(a))
+
+var string = ''
+var o = {lato1: 8, lato2: 3}
+var a = Object.keys(o)
+console.log(a)
+string += a[0];
+console.log(string)
+var tmp = ''
+tmp = a[0]
+string += o.tmp;
+console.log(o[tmp])
 */
+
 
 module.exports = {getArea}
